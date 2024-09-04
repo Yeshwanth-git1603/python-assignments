5eceaffd839ada0899708939077b3051c818a346
EC2 Default User <ec2-user@ip-172-31-18-79.ec2.internal>
2024-09-04 10:07:41 +0000

    python

---
+++ typecast_method/example1.py
@@ -0,0 +1,14 @@
+# type casting method
+
+#type casting method is used to type caste the given type to another type
+
+x=10
+print(str(x))
+y=["yeshwanth"]
+print(type(y))
+print(str(y))
+print(type(y))
+print(y)
+print(float(x))
+print(bool(x))
+print(tuple(y))
